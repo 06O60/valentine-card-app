f1dca8b52e2c07388bed8a8c462c7a405a8252eb
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2024-02-07 22:35:41 +0100

    Clean up boilerplate code and files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './assets/styles/App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<main>
+				<p>There will be a valentine's day card here soon!</p>
+			</main>
+		</div>
+	);
+}
+
+export default App;
