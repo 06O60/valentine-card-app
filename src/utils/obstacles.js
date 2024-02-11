f7e47f1866e9d4b7819028b7bac5de53271ca301
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2024-02-11 20:44:00 +0100

    Add swapping the yes and no buttons place

--- src/utils/obstacles.js
+++ src/utils/obstacles.js
@@ -1,10 +1,19 @@
-export function makeYesButtonBigger(button1, button2) {
-	let button = button1;
-	if (button1.innerText !== 'Yes') button = button2;
+const obstacles = [
+	function makeYesButtonBigger(button1, button2) {
+		let button = button1;
+		if (button1.innerText !== 'Yes') button = button2;
 
-	const initialWidth = button.offsetHeight;
-	const initialHeight = button.offsetWidth;
-	const sizeIncrease = Math.random() * 50;
-	button.style.height = `${initialWidth + sizeIncrease / 2}px`;
-	button.style.width = `${initialHeight + sizeIncrease}px`;
-}
+		const initialWidth = button.offsetHeight;
+		const initialHeight = button.offsetWidth;
+		const sizeIncrease = Math.random() * 50;
+		button.style.height = `${initialWidth + sizeIncrease / 2}px`;
+		button.style.width = `${initialHeight + sizeIncrease}px`;
+	},
+	function switchButtons(button1, button2) {
+		const tmp = button1.innerText;
+		button1.innerText = button2.innerText;
+		button2.innerText = tmp;
+	}
+];
+
+export default obstacles;
