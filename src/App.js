ea9428a7b1f1169971ea16ae406e113cc5822699
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2024-02-11 15:43:51 +0100

    Create a framework for handling button click events.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import './assets/styles/App.css';
 import TextBox from './components/TextBox.js';
 import MyButton from './components/MyButton.js';
 import Character from './components/Character.js';
-function App() {
+import { useState } from 'react';
+
+function App({ messages, messagesTypes }) {
+	const [message, setMessage] = useState('Will you be my valentine...?');
+
+	function handleClick(buttonMsg) {
+		if (buttonMsg === 'Yes') {
+			console.log('Yay! Thank you!');
+			showEnd();
+		} else {
+			console.log('No clicked...');
+			changeDialog();
+			changeSprite();
+		}
+	}
+	function showEnd() {
+		console.log('Show end TBI');
+	}
+	function changeDialog() {
+		console.log('TBI');
+	}
+	function changeSprite() {
+		console.log('TBI');
+	}
+
 	return (
 		<div className="App">
 			<main>
@@ -11,10 +35,7 @@ function App() {
 					<MyButton text="No" />
 				</div>
 				<Character />
-				<TextBox
-					character="Monika"
-					text="Will you be my valentine...?"
-				/>
+				<TextBox character="Monika" text={message} />
 			</main>
 		</div>
 	);
