41f1e817e3d0bf2419bb2f604149229a1d291267
Aleksandra Kierska <aleksandrakierska8@gmail.com>
2024-02-12 14:54:25 +0100

    Add glitching character animation

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/styles/character.css';
 import monicaSideways from '../assets/images/normal/monica-sideways.webp';
+import { type } from '@testing-library/user-event/dist/type';
 const spritesPath = '../assets/images/normal/';
 const glitchedSpritesPath = '../assets/images/glitched';
 
@@ -9,6 +10,7 @@ const characterSprites = {
 	confident: require('../assets/images/normal/monica-front-confident.png'),
 	lecturing: require('../assets/images/normal/monica-lecturing.webp'),
 	glitched: [
+		require('../assets/images/glitched/monica-soft-glitch.webp'),
 		require('../assets/images/glitched/monica-glitch-1.webp'),
 		require('../assets/images/glitched/monica-glitch-2.webp'),
 		require('../assets/images/glitched/monica-glitch-3.webp')
@@ -25,10 +27,33 @@ const Character = ({ currentSprite = characterSprites.sideways.flirty }) => {
 		/>
 	);
 };
+
+let glitchInterval;
+let glitchTimeout;
+function glitchAnimation(setSprite) {
+	let currentIndex = -1;
+	glitchInterval = setInterval(() => {
+		currentIndex =
+			(currentIndex + 1) % (characterSprites['glitched'].length - 1);
+		setSprite(characterSprites['glitched'][currentIndex + 1]);
+	}, 300);
+}
+
 /* mabye add possibility to have more than 1 sprite per type?*/
 function setSpriteByType(typeDescription, setSprite) {
-	const posture = typeDescription.posture;
-	setSprite(characterSprites[posture]);
+	//if there's a glitch animation going on
+	//from the previous dialog, stop it.
+	if (glitchInterval) clearInterval(glitchInterval);
+	if (glitchTimeout) clearTimeout(glitchTimeout);
+
+	if (Math.random() <= typeDescription.glitchChance) {
+		console.log('Here');
+		setSprite(characterSprites['glitched'][0]);
+		glitchTimeout = setTimeout(() => glitchAnimation(setSprite), 350);
+	} else {
+		const posture = typeDescription.posture;
+		setSprite(characterSprites[posture]);
+	}
 }
 
 export { setSpriteByType };
